115c1f968f049e419fcf76573e00574a1d17fe81
02Harshit <138508265+02Harshit@users.noreply.github.com>
2026-02-08 12:22:06 +0530

    Time: 4 ms (49.17%), Space: 45.5 MB (62.94%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,21 +1,39 @@
 class Solution {
-    private static int getMinimum(List<List<Integer>> triangle,int i,int j,int dp[][]) {
-        int rows = triangle.size();
-        if(i == rows-1) return (triangle.get(i)).get(j);
-        if (dp[i][j] != Integer.MAX_VALUE) return dp[i][j];
+    // private static int getMinimum(List<List<Integer>> triangle,int i,int j,int dp[][]) {
+    //     int rows = triangle.size();
+    //     if(i == rows-1) return (triangle.get(i)).get(j);
+    //     if (dp[i][j] != Integer.MAX_VALUE) return dp[i][j];
 
-        int down = getMinimum(triangle,i+1,j,dp);
-        int diagonal = getMinimum(triangle,i+1,j+1,dp);
+    //     int down = getMinimum(triangle,i+1,j,dp);
+    //     int diagonal = getMinimum(triangle,i+1,j+1,dp);
 
-        return dp[i][j] = ((triangle.get(i)).get(j) + Math.min(down,diagonal)); 
-    }
+    //     return dp[i][j] = ((triangle.get(i)).get(j) + Math.min(down,diagonal)); 
+    // }
     public int minimumTotal(List<List<Integer>> triangle) {
         int rows = triangle.size();
         int cols = triangle.get(rows-1).size();
         int dp[][] = new int[rows][cols];
-        for(int i = 0; i<rows; i++) {
-            Arrays.fill(dp[i],Integer.MAX_VALUE);
-        } 
-        return getMinimum(triangle,0,0,dp);
+        if (rows == 1) return triangle.get(0).get(0);
+        int minPathSum = Integer.MAX_VALUE;
+        // for(int i = 0; i<rows; i++) {
+        //     Arrays.fill(dp[i],Integer.MAX_VALUE);
+        // }
+        dp[0][0] = triangle.get(0).get(0);
+        for(int i=1; i<rows; i++) {
+            for(int j=0; j<=i; j++) {
+                    
+                int up = Integer.MAX_VALUE;
+                int diagonal = Integer.MAX_VALUE;
+                if (j < i) up = dp[i-1][j];
+                if (j > 0) diagonal = dp[i-1][j-1];
+
+                dp[i][j] = triangle.get(i).get(j) + Math.min(up,diagonal);
+                if (i == rows-1) {
+                    minPathSum = Math.min(minPathSum,dp[i][j]);
+                }
+            }
+        }
+
+        return minPathSum;
     }
 }
