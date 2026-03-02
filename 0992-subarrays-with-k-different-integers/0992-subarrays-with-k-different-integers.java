ffddd8fe8509e4e833dbc87a49d1a305e0212a04
02Harshit <138508265+02Harshit@users.noreply.github.com>
2026-03-02 14:56:58 +0530

    Time: 48 ms (75.42%), Space: 50.1 MB (67.31%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,26 +1,25 @@
 class Solution {
-    private static int subarrayWithatMostKDistinct(int nums[], int k) {
-        int l=0, count = 0,n=nums.length;
-        HashMap<Integer,Integer> map = new HashMap<>(); 
+    public int subarraysWithAtMostKDistinct(int[] nums, int k) {
+        if (k == 0) return 0;
+        int count = 0,l=0,n=nums.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
         
-        for(int r=0; r<n; r++) {
+        for(int r = 0; r<n; r++) {
             map.put(nums[r],map.getOrDefault(nums[r],0)+1);
 
             while (map.size() > k) {
                 map.put(nums[l],map.get(nums[l])-1);
-                if (map.get(nums[l]) == 0) map.remove(nums[l]);
+                if(map.get(nums[l]) == 0) map.remove(nums[l]);
                 l++;
             }
 
-            if (map.size() <= k) count+= r-l+1;
+            count += r-l+1;
         }
 
         return count;
     }
 
     public int subarraysWithKDistinct(int[] nums, int k) {
-        // System.out.println(subarrayWithatMostKDistinct(nums,k));
-        // System.out.println(subarrayWithatMostKDistinct(nums,k-1));
-        return  subarrayWithatMostKDistinct(nums,k) - subarrayWithatMostKDistinct(nums,k-1);
-    }
+        return subarraysWithAtMostKDistinct(nums,k) - subarraysWithAtMostKDistinct(nums,k-1);
+    } 
 }
